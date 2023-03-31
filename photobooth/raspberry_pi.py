b89cc6358e9bf4e10dc3b61d0f0ca226887bafc1
Corentin Gitton <cgitton@alkemics.com>
2023-03-31 18:14:57 +0200

    feat: improve stability of raspberry process

--- photobooth/raspberry_pi.py
+++ photobooth/raspberry_pi.py
@@ -14,29 +14,32 @@ leds = [
 ]
 
 
-def setup():
+def reset_leds():
     for led in leds:
         led.off()
 
 
 def capture_3_photos():
     paths = []
-    for i in range(0, 3):
-        leds[i].blink(on_time=0.5, off_time=0.5, n=WAIT_BETWEEN_PHOTOS)
-        sleep(WAIT_BETWEEN_PHOTOS)  # Wait X seconds
-        leds[i].on()
 
-        local_path = capture_image()
-        paths.append(local_path)
+    try:
+        for i in range(0, 3):
+            leds[i].blink(on_time=0.5, off_time=0.5, n=WAIT_BETWEEN_PHOTOS)
+            sleep(WAIT_BETWEEN_PHOTOS)  # Wait X seconds
+            leds[i].on()
 
-    for led in leds:
-        led.off()
+            local_path = capture_image()
+            paths.append(local_path)
+    except Exception:
+        pass
+    finally:
+        reset_leds()
 
     print(paths)
 
 
 if __name__ == "__main__":
-    setup()
+    reset_leds()
     button.when_activated = capture_3_photos
     print('Ready. To start taking pictures, press on the button')
     pause()
